599f5f427c23c4ef5d78db9264648702a1748a58
kelson.gardner <kelson.gardner14@gmail.com>
2024-08-12 22:33:16 -0600

    Got rid of unnecessary service worker in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/Entries/sw.js', {scope: '/Entries/'}, {type: 'module'}).then(registration => {
-      console.log('ServiceWorker registered: ', registration);
-    }).catch(error => {
-      console.log('ServiceWorker registration failed: ', error);
-    });
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -32,10 +32,6 @@ export default defineConfig({
           },
         ],
       },
-})
+    })
   ],
-  server: {
-    host: '0.0.0.0',
-    port: 8080
-  }
 })
